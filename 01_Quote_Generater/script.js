2ec9ba91e3a3af721d336ce9e600e92ef2a029a1
nicolakacha <nicolakacha@gmail.com>
2020-08-09 20:59:01 +0800

    project1

--- 01_Quote_Generater/script.js
+++ 01_Quote_Generater/script.js
@@ -1,17 +1,17 @@
-const quoteContainer = document.querySelector("#quote-container");
-const quoteText = document.querySelector("#quote");
-const authorText = document.querySelector("#author");
-const twitterBtn = document.querySelector("#twitter");
-const newQuoteBtn = document.querySelector("#new-quote");
-const loader = document.querySelector("#loader");
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-// Show Loading
+// Loading Spinner Shown
 function loading() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// Hide Loading
+// Remove Loading Spinner
 function complete() {
   if (!loader.hidden) {
     quoteContainer.hidden = false;
@@ -19,48 +19,46 @@ function complete() {
   }
 }
 
-//Get Quote
-function getQuote(data) {
-  if (data.quoteAuthor === "") {
-    authorText.innerText = "Unknown";
-  } else {
-    authorText.innerText = data.quoteAuthor;
+// Get Quote From API
+async function getQuote() {
+  loading();
+  // We need to use a Proxy URL to make our API call in order to avoid a CORS error
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // Dynamically reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
+    // Stop Loading, Show Quote
+    complete();
+  } catch (error) {
+    getQuote();
   }
-  if (data.quoteText.length > 120) {
-    quoteText.classList.add("long-quote");
-  } else {
-    quoteText.classList.remove("long-quote");
-  }
-  quoteText.innerText = data.quoteText;
 }
 
+// Tweet Quote
 function tweetQuote() {
   const quote = quoteText.innerText;
   const author = authorText.innerText;
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-  window.open(twitterUrl, "_blank");
+  window.open(twitterUrl, '_blank');
 }
 
-// Get Quote From API
-async function getData() {
-  const proxyUrl = "http://mighty-gorge-35380.herokuapp.com/";
-  const apiUrl =
-    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
-  try {
-    loading()
-    const response = await fetch(proxyUrl + apiUrl);
-    const data = await response.json();
-    getQuote(data);
-    complete()
-  } catch (error) {
-    console.log(error);
-    getData();
-  }
-}
-
-//Event Listeners
-newQuoteBtn.addEventListener("click", getData);
-twitterBtn.addEventListener("click", tweetQuote);
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
-getData();
+getQuote();
